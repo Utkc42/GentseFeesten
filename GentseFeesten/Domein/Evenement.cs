2b41bc60d039c0a0644b9287633163deceef436e
Muhammed <91887062+Utkc42@users.noreply.github.com>
2022-12-22 12:38:44 +0100

    Solution aangemaakt met 2 klassen van het domein

---
+++ GentseFeesten/Domein/Evenement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domein
+{
+    public class Evenement
+    {
+        public int Id { get; set; }
+        public string Naam { get; set; }
+        public int Prijs { get; set; }
+        public int Stardatum { get; set; }
+        public int Einddatum { get; set; }
+    }
+}
